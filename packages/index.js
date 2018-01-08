f2f8374a82d6990db47811db5b3eaadce3c3f920
Michael Casey <mcasey8540@gmail.com>
2018-01-08 16:07:13 -0500

    Add index.js with insertionSortArray function

---
+++ packages/index.js
@@ -0,0 +1,22 @@
+/**
+ * Sort an array via insertion sort, pass by value
+ * 
+ * @param  {Array} arr - The array to be sorted.
+ * @return {Array} - The sorted array.
+ */
+
+const insertionSortArray = (arrayToSort)=>{
+	let arr = arrayToSort.slice(0)
+	for(var j = 1; j < arr.length; j++){
+		let key = arr[j];
+		let i = j - 1;
+		while(i>-1 && arr[i] > key){
+			arr[i + 1] = arr[i];
+			i = i - 1;
+		}
+		arr[i+1]=key;
+	}
+	return arr;
+}
+
+module.exports = insertionSortArray;
