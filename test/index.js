112a21eca07c99c5a2eb8f7708c8983eea52c454
Michael Casey <mcasey8540@gmail.com>
2018-01-08 16:18:06 -0500

    Update ava test case

---
+++ test/index.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+const insertionSort = require('../packages/index');
+
+test(`sorts an array of numbers`, t => {
+	let arr = [5, 2, 4, 6, 1, 3];
+	t.deepEqual(insertionSort(arr), [1, 2, 3, 4, 5, 6]);
+});
+
+test(`sorts an empty array returns an empty array`, t => {
+	let arr = [];
+	t.deepEqual(insertionSort(arr), []);
+});
+
